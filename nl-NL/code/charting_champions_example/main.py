a088ed381ee1f066863477b29082fc4ec77e8549
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-06-07 16:09:14 +0100

    New translations main.py (Dutch)

--- nl-NL/code/charting_champions_example/main.py
+++ nl-NL/code/charting_champions_example/main.py
@@ -2,20 +2,20 @@
 from pygal import Bar
 
 # Maak een grafiek
-chart = Bar(title='Olympische medailles')
+grafiek = Bar(title='Olympische medailles')
 
 # Voeg gegevens toe aan de grafiek
 with open('medals.csv') as f:
     data = f.read()
-    lines = data.splitlines()
-    # print(lines)
+    regels = data.splitlines()
+    # print(regels)
 
-    for line in lines:
-        totaal = line.split(',')
+    for regel in regels:
+        totaal = regel.split(',')
         # print(totaal)
         team = totaal[0]
         medailles = totaal[1]
         chart.add(team,int(medailles)) # Maak van medailles een getal
 
 # Toon de grafiek
-chart.render()
+grafiek.render()
